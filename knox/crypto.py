f2cd187300043c909c76e54783c1be071cd0e207
James <James>
2016-08-15 16:49:49 +0100

    beginning to use pbkdf2 for multi-pass sha512. Need to inspect using cryptography for auth rather than manually comparing hashes

--- knox/crypto.py
+++ knox/crypto.py
@@ -2,11 +2,15 @@ import binascii
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from OpenSSL.rand import bytes as generate_bytes
 
 from knox.settings import knox_settings, CONSTANTS
 
+backend = default_backend()
 sha = knox_settings.SECURE_HASH_ALGORITHM
+
+
 
 def create_token_string():
     return (
@@ -29,7 +33,11 @@ def hash_token(token, salt):
     Calculates the hash of a token and salt.
     input is unhexlified
     '''
-    digest = hashes.Hash(sha(), backend=default_backend())
-    digest.update(binascii.unhexlify(token))
-    digest.update(binascii.unhexlify(salt))
+    kdf = PBKDF2HMAC(
+        algorithm=sha(),
+        length=int(knox_settings.AUTH_TOKEN_CHARACTER_LENGTH/2),
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
     return binascii.hexlify(digest.finalize()).decode()
